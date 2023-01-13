a0a668e5152043abfe2ac444f91f68a23e996962
Cooper Kozitza <cooperkozitza@gmail.com>
2023-01-13 13:17:43 -0800

    Added Checkpoints, Respawning, and Camera Zoom

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,11 +5,27 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     public MovementScripts movementScript;
-    public Transform target;
+
+    [Range(-1, 1)]
+    public float zoom = 1.0f;
+    public float zoomOffset = 0;
+
+    [Range(0, 10)]
+    public float verticalCameraDistance = 0;
+
+    private Vector3 startPos;
+    private float starDistance = 0;
+
+    private void Start()
+    {
+        startPos = transform.position;
+        starDistance = Vector3.Distance(movementScript.rigidbodies[0].transform.position, movementScript.rigidbodies[1].transform.position);
+    }
 
     private void Update()
     {
         Vector3 pos = Vector3.Lerp(movementScript.rigidbodies[0].transform.position, movementScript.rigidbodies[1].transform.position, 0.5f);
-        target.position = new Vector3(pos.x, target.position.y, target.position.z);
+        transform.position = new Vector3(pos.x, startPos.y, startPos.z);
+        transform.Translate(new Vector3(0, 0, startPos.z - (Vector3.Distance(movementScript.rigidbodies[0].transform.position, movementScript.rigidbodies[1].transform.position) - starDistance) * zoom + zoomOffset), Space.Self);
     }
 }
---
+++ Assets/Scripts/Respawn.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour
+{
+    private Vector3 respawnPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        respawnPos = transform.position;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Deathzone"))
+        {
+            Debug.Log(respawnPos.x.ToString() + " " + respawnPos.y.ToString());
+            transform.position = respawnPos;
+        }
+        else if (other.CompareTag("Checkpoint"))
+        {
+            respawnPos = other.transform.position;
+        }
+    }
+}
